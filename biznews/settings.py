2cb90206f43077699e075fb5510da4c9fd739e4f
s13g <ayflix0@gmail.com>
2022-10-22 21:53:04 +0100

    import

--- biznews/settings.py
+++ biznews/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+# import libraries
+
 from pathlib import Path
 import os
 import cloudinary
